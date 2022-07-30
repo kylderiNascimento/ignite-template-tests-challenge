9cd770585a0e50844a431d2064715737dc7b127b
KylderiNascimento <kylderi.nascimento@hotmail.com>
2022-07-30 07:05:24 -0300

    resolvido

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,19 +1,20 @@
 import { inject, injectable } from "tsyringe";
-import { hash } from 'bcryptjs';
+import { hash } from "bcryptjs";
 
 import { CreateUserError } from "./CreateUserError";
 
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserDTO } from "./ICreateUserDTO";
+import { User } from "@modules/users/entities/User";
 
 @injectable()
 export class CreateUserUseCase {
   constructor(
-    @inject('UsersRepository')
-    private usersRepository: IUsersRepository,
-  ) {}
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) { }
 
-  async execute({ name, email, password }: ICreateUserDTO) {
+  async execute({ name, email, password }: ICreateUserDTO): Promise<User> {
     const userAlreadyExists = await this.usersRepository.findByEmail(email);
 
     if (userAlreadyExists) {
